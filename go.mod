8c7676678d45201f9faa1eb85845c7a33a6279d3
Mark Belaev <markbelaevv@gmail.com>
2025-08-23 00:08:53 +0500

    Добавил таблицу и миграции

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.5
 require (
 	github.com/gin-gonic/gin v1.10.1
 	github.com/jackc/pgx/v5 v5.7.5
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
---
+++ migrations/000001_create_table_todo.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE todos (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status BOOLEAN DEFAULT false,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
